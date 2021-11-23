2284625c457f9f18d63a02af17781177fafb39bb
rameshJain69 <77069758+rameshJain69@users.noreply.github.com>
2021-11-23 09:32:08 +0530

    Update EarthquakeReducer.java

--- earthquake/EarthquakeReducer.java
+++ earthquake/EarthquakeReducer.java
@@ -11,17 +11,6 @@ public class EarthquakeReducer extends
         Reducer<Text, DoubleWritable, Text, DoubleWritable> 
 {
 
-    /**
-     * The `Reducer` function. Iterates through all earthquake magnitudes for a
-     * region to find the maximum value. The output key is the `region name` and  
-     * the value is the `maximum magnitude` for that region.
-     * @param key - Input key - Name of the region
-     * @param values - Input Value - Iterator over quake magnitudes for region
-     * @param context - Used for collecting output
-     * @throws IOException
-     * @throws InterruptedException 
-     */
-    @Override
     public void reduce(Text key, Iterable<DoubleWritable> values, 
             Context context) throws IOException, InterruptedException {
         
